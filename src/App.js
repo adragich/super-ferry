2b4c8e53880376976c8e43e2dc43346e34ffc05d
Anastasiia Dragich <anastasiia.dragich@booking.com>
2022-04-07 21:16:58 +0200

    ferry animation

--- src/App.js
+++ src/App.js
@@ -4,40 +4,62 @@ import React, { useEffect, useState, useRef } from "react";
 import * as ReactDOM from 'react-dom';
 
 function App() {
-  const [top, setTop] = React.useState(Math.floor(Math.random() * 100))
-  const [left, setLeft] = React.useState(Math.floor(Math.random() * 75))
-  const [width, setWidth] = React.useState(1000) //x
-  const [height, setheight] = React.useState(1000) //y
+  const DELAY = 1500;
+  // const [top, setTop] = React.useState(Math.floor(Math.random() * 100))
+  // const [left, setLeft] = React.useState(Math.floor(Math.random() * 75))
+  const [top, setTop] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [topDirection, setTopDirection] = useState(1)
+  const [leftDirection, setLeftDirection] = useState(1)
+  const MAX_WIDTH = window.innerWidth;
+  const MAX_HEIGHT = window.innerHeight;
+  const ELEMENT_HEIGHT = 100;
+  const ELEMENT_WIDTH = 100;
 
-  function changePosition(number = 5) {
-    const newPositionX = Math.floor(Math.random() * number);
-    const newPositionY = Math.floor(Math.random() * number);
-    if (top !== width) {
-      setTop(top + newPositionY)
+  function changePosition(number = 200) {
+    const newPositionX = (Math.floor(Math.random() * number) + 100) * leftDirection;
+    const newPositionY = (Math.floor(Math.random() * number) + 100) * topDirection;
+
+    if (top + ELEMENT_HEIGHT + newPositionY - MAX_HEIGHT >= 0) {
+      setTopDirection(-1);
+    } else if (top - ELEMENT_HEIGHT + newPositionY <= 0) {
+      setTopDirection(1);
     } else {
-      setTop(top - newPositionY)
+      
     }
 
-    if (left !== height) {
-      setLeft(left + newPositionX)
-    } else {
-      setLeft(left - newPositionX)
+    if (left + ELEMENT_WIDTH + newPositionX - MAX_WIDTH >= 0) {
+      setLeftDirection(-1);
+    } else if (left - ELEMENT_WIDTH + newPositionX <= 0) {
+      setLeftDirection(1);
     }
+
+    setTop(top + newPositionY)
+    setLeft(left + newPositionX)
   }
 
   useEffect(() => {
-    const interval = setInterval(changePosition, 10);
+    const interval = setInterval(changePosition, DELAY);
+
     return () => clearInterval(interval)
   });
 
   return (
-    <div>{top}, {left}
-      <div id='ferry' style={{
-        transform: `translate(${top}px, ${left}px)`
-      }}>Ferry</div>
-      <div id='ferry' style={{
-        transform: `translate(${top}px, ${left}px)`
-      }}>Ferry</div>
+    <div className="box-drift">
+      <div 
+        id='ferry'
+        className="box-bob"
+        style={{
+          transform: `translate(${left}px, ${top}px)`,
+          transition: `${DELAY}ms cubic-bezier(0.31, 0.51, 0.54, 1.29) 0s`,
+        }}
+      >
+        <div className="box-pitch">
+          <div className="left-sail"></div>
+          <div className="right-sail"></div>
+          <div className="hull"></div>
+        </div>
+      </div>
     </div>
   );
 }
